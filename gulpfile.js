76d603357279fc35dc9780fc12075ecebd1f511c
ybeliaev <ybeliaev@gmail.com>
2019-01-11 15:39:21 +0200

    основные настройки сборки проекта

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@
         
     //gulp-sass
     gulp.task('sass', function(){ // Создаем таск "sass"
-        return gulp.src('app/sass/*.sass') // Берем источник
+        return gulp.src('app/sass/main.scss') // Берем источник
             .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
             .pipe(autoprefixer(['last 2 versions'], { cascade: true })) // Создаем префиксы
             .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
@@ -28,7 +28,7 @@
     });
     //minification
     gulp.task('cssnano', function() {
-    return gulp.src('app/css/libs.css')// в libs.css @import
+    return gulp.src('app/css/main.css')// в libs.css @import
         .pipe(cssnano()) // Сжимаем
         .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
         .pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
@@ -49,7 +49,7 @@
         .pipe(clean());
 });
     gulp.task('watch',['browser-sync', 'sass', 'scripts','cssnano'], function() {
-        gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами
+        gulp.watch('app/sass/**/*.scss', ['sass']); // Наблюдение за sass файлами
         gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
         gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
 
@@ -65,7 +65,7 @@
 
         var buildCss = gulp.src([ // Переносим CSS стили в продакшен
             'app/css/main.css',
-            'app/css/libs.min.css'
+            'app/css/style.min.css'
             ])
         .pipe(gulp.dest('dist/css'))
 
